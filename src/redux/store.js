cd8ee167b79826fb71988b206c83f7e1b67bfa70
ilef-bahrouni <ilef.bahrouni2002@egmail.com>
2024-08-26 12:13:17 +0200

    v2

--- src/redux/store.js
+++ src/redux/store.js
@@ -22,5 +22,5 @@ const store = configureStore({
 
 export default store;
 
- export const server = 'http://localhost:5000/api/v1';
-//export const server = 'https://e-learning-server-flame.vercel.app/api/v1'
+// export const server = 'http://localhost:5000/api/v1';
+export const server = 'https://e-learning-server-cyan.vercel.app/api/v1'
